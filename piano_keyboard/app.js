338300a116f2c868b25c538d074a9215834bb511
Nirmit Sawhney <nirmitsawhney01@gmail.com>
2022-06-20 11:52:33 +0530

    changes commit

---
+++ piano_keyboard/app.js
@@ -0,0 +1,6 @@
+let whiteKeys=document.querySelectorAll(".white-keys");
+
+whiteKeys.addEventListener("click",()=>{
+    let audio=document.createElement("audio");
+    audio.setAttribute("src","./key-1.mp3");
+})
