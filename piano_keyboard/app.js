eda2c291588c891643193353ac7ab5995dde9145
Nirmit Sawhney <nirmitsawhney01@gmail.com>
2022-06-20 15:42:45 +0530

    done changes

--- piano_keyboard/app.js
+++ piano_keyboard/app.js
@@ -1,12 +1,12 @@
-const playAudio=(idx)=> () => {
-    const audio=new Audio(`./audio/key-${idx+1}.mp3`);
+const playAudio = (idx) => () => {
+    const audio = new Audio(`./audio/key-${idx + 1}.mp3`);
     audio.play();
 };
 
-const pianoFunctionality=()=>{
-    const keys=document.querySelectorAll("a");
-    keys.forEach((elem,idx)=>{
-        elem.addEventListener("click",playAudio(idx));
+const pianoFunctionality = () => {
+    const keys = document.querySelectorAll("a");
+    keys.forEach((elem, idx) => {
+        elem.addEventListener("click", playAudio(idx));
     });
 };
 
