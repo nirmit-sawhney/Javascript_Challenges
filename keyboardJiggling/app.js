917ad586379199eab5b37f74964853e39842a18a
Nirmit Sawhney <nirmitsawhney01@gmail.com>
2022-06-24 17:46:05 +0530

    testing

--- keyboardJiggling/app.js
+++ keyboardJiggling/app.js
@@ -1,36 +1,39 @@
 let keys = document.getElementsByClassName("key");
 let randomKey, dataKey;
 
-const getRandomKey=(min,max)=>{
-    return Math.floor(Math.random() * (max - min) ) + min;
-}
+const getRandomKey = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const jiggleRandomKey = () => {
-    randomKey = getRandomKey(0,keys.length);
+    randomKey = getRandomKey(0, keys.length);
     keys[randomKey].classList.add("jiggle");
     dataKey = keys[randomKey].getAttribute("data-key");
-}
+};
 
 jiggleRandomKey();
 
-document.addEventListener("keydown", (event) => {
-    let pressedKey=event.key.toUpperCase();
-    if(pressedKey=="TAB") {
-        event.preventDefault();
-    }
-    if (pressedKey == dataKey) {
-        keys[randomKey].classList.remove("jiggle");
-        jiggleRandomKey();
-    }else{
-        showErrow(pressedKey);
-    }
-}, true);
+document.addEventListener(
+    "keydown",
+    (event) => {
+        let pressedKey = event.key.toUpperCase();
+        if (pressedKey == "TAB") {
+            event.preventDefault();
+        }
+        if (pressedKey == dataKey) {
+            keys[randomKey].classList.remove("jiggle");
+            jiggleRandomKey();
+        } else {
+            showErrow(pressedKey);
+        }
+    },
+    true
+);
 
-const showErrow=(pressedKey)=>{
-    let elem=document.querySelector(`button[data-key='${pressedKey}']`);
+const showErrow = (pressedKey) => {
+    let elem = document.querySelector(`button[data-key='${pressedKey}']`);
     elem.classList.add("showerror");
-    setTimeout(()=>{
+    setTimeout(() => {
         elem.classList.remove("showerror");
-    },300);
-}
-
+    }, 300);
+};
--- pomodoro_clock/script.js
+++ pomodoro_clock/script.js
@@ -1,30 +1,36 @@
-let start=document.querySelector(".start");
-let min=document.querySelector('#min');
-let sec=document.querySelector("#sec");
-let ring=document.querySelector('.ring');
-let settings=document.querySelector('.settings');
+let start = document.querySelector(".start");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+let ring = document.querySelector(".ring");
+let settings = document.querySelector(".settings");
 
-import { stopTimer,startTimer, allowInp,disableInp, validate } from "./utility.js";
+import {
+    stopTimer,
+    startTimer,
+    allowInp,
+    disableInp,
+    validate
+} from "./utility.js";
 
-settings.addEventListener('click',()=>{
+settings.addEventListener("click", () => {
     stopTimer();
     allowInp();
-})
+});
 
-start.addEventListener('click',()=>{
+start.addEventListener("click", () => {
     disableInp();
-    if(start.innerHTML=="start"){
-        if(validate()){
-            alert('Please Enter a valid Time');
-            min.value="00";
-            sec.value="00";
-            ring.style.stroke="#09A65A";
-        }else{
+    if (start.innerHTML == "start") {
+        if (validate()) {
+            alert("Please Enter a valid Time");
+            min.value = "00";
+            sec.value = "00";
+            ring.style.stroke = "#09A65A";
+        } else {
             startTimer();
         }
-    }else{
+    } else {
         stopTimer();
     }
-})
+});
 
-export {start,min,sec,ring};
+export { start, min, sec, ring };
